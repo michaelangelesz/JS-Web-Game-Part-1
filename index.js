2a9fecc7ba48a3e617c00120a3eb8803665a09cb
michaelangelesz <mmweisphal@outlook.com>
2023-05-09 20:24:40 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+newImage('assets/pine-tree.png', 450, 200)
+
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', function(){
+        object.remove()
+    })
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+tile('assets/grass.png', 0, 0)
+tile('assets/sky.png', 0, 500)
+
+for tile(let y = 0; y < window.innerHeight; y += imageHeight) {
+    for (let x = 0; x < window.innerWidth; x += imageWidth) {
+      const img = document.createElement('img');
+      img.src = imageUrl;
+      img.style.position = 'fixed';
+      img.style.left = x + 'px';
+      img.style.top = y + 'px';
+      document.body.append(img);
+    }
+  }
+
+//let grassHeight =window.innerheight-500
+//let skyHeight =window.innerHeight -100
